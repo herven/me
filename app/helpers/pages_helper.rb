dbd60472ee4cef0a5cbd457cc852bad008c084c9
Raphael Nti <ralphinc@MacBook-Pro-de-Raphael.local>
2013-01-24 00:31:07 +0100

    litle refactoring of my pages_helper.rb

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -4,14 +4,14 @@ module PagesHelper
 	def flag_show(language)
 		content_tag(:ul, class: "nav pull-right") do
 			if language == 'ru'
-				content_tag(:li, link_to( image_tag('fr.png'), params.except(:controller, :action).merge(locale: 'fr') )) +
-				content_tag(:li, link_to( image_tag('en.png'), params.except(:controller, :action).merge(locale: 'en') ))
+				content_tag(:li, link_to( image_tag('fr.png'), params.merge(locale: 'fr') )) +
+				content_tag(:li, link_to( image_tag('en.png'), params.merge(locale: 'en') ))
 			elsif language == 'en'
-				content_tag(:li, link_to( image_tag('fr.png'), params.except(:controller, :action).merge(locale: 'fr') )) +
-				content_tag(:li, link_to( image_tag('ru.png'), params.except(:controller, :action).merge(locale: 'ru') ))
+				content_tag(:li, link_to( image_tag('fr.png'), params.merge(locale: 'fr') )) +
+				content_tag(:li, link_to( image_tag('ru.png'), params.merge(locale: 'ru') ))
 			else
-				content_tag(:li, link_to( image_tag('en.png'), params.except(:controller, :action).merge(locale: 'en') )) +
-				content_tag(:li, link_to( image_tag('ru.png'), params.except(:controller, :action).merge(locale: 'ru') ))
+				content_tag(:li, link_to( image_tag('en.png'), params.merge(locale: 'en') )) +
+				content_tag(:li, link_to( image_tag('ru.png'), params.merge(locale: 'ru') ))
 			end
 		end
 	end
